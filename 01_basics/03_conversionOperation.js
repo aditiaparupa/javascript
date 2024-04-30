0f599cf88ae60ef9917c3d9920df7480823a8909
aditiaparupa <168429899+aditiaparupa@users.noreply.github.com>
2024-04-30 11:49:15 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,11 +10,37 @@
 //true=1,false=0
 //""=false
 //"aditi"=true
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// let value=3
+// negValue=-value
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3*3);
+// console.log(2**3);
+// let str1=" hello "
+// let str2=" aditi "
+// let str3=str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
