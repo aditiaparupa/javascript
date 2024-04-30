3faeadf6dc26ad8a1a6833e0430dcc2d31424357
aditiaparupa <168429899+aditiaparupa@users.noreply.github.com>
2024-04-30 14:09:45 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,23 @@
+// #primitive
+//7 types:string,number,boolean,null,undefined,symbol,bigInt
+// const score=100
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+// const id=symbol('123')
+// const anotherId=symbol('123')
+// console.log(id ===anotherId);
+// const bigNumber=234552746864939n
+//reference(non primitive)
+//array,objects,functions
+
+const heros=["aditi","riya","siya"];
+let obj={
+    name:"aditi",
+    age:20,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
+ console.log(typeof myFunction);
