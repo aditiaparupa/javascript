325135bdb890487237a10d60dc76e588076778cc
aditiaparupa <168429899+aditiaparupa@users.noreply.github.com>
2024-04-30 11:29:10 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score =null
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueIntNumber=Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+//'33'=33
+//"33abc"=NaN
+//true=1,false=0
+//""=false
+//"aditi"=true
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
