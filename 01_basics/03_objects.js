d0b5390383ea5a52fcbf768c7aac25e315e96fee
aditiaparupa <168429899+aditiaparupa@users.noreply.github.com>
2024-05-08 10:47:57 +0000

    function

---
+++ 01_basics/03_objects.js
@@ -0,0 +1,17 @@
+const jsUser={
+
+    name:"aditi",
+    "full name":"aditi aparupa",
+    [mySym]:"mykey1",
+    age:20,
+    location:"bbsr",
+    email:"aditi@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday","saturday"]
+}
+
+// console.log(jsUser["email"]);
+console.log(jsUser.email)
+console.log(jsUser["email"])
+console.log(jsUser["full name"])
+console.log(jsUser[mySym])
---
+++ 01_basics/functions.js
@@ -0,0 +1,42 @@
+// console.log("a");
+// console.log("d");
+
+function sayMyName()
+{
+//     console.log("a");
+// console.log("d");
+
+}
+
+//sayMyName()
+
+function addTwoNumbers(number1,number2){
+    //console.log(number1+number2);
+}
+function addTwoNumbers(number1,number2){
+//     let result=number1+number2
+//     return result
+return number1+number2
+}
+
+
+
+const result=addTwoNumbers(3,4)
+//console.log("Result:",result);
+
+
+function loginUserMessage(username){
+    if(username===undefined){
+       // console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+//console.log(loginUserMessage("aditi"))
+
+function calculateCartPrice(num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,5000))
